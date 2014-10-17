c3c06958a3d1a63364cfd5ca72cf88f0d0025813
bogdan.teut <bogdan_teut@yahoo.com>
2014-10-17 17:42:25 +0100

    adding manual toasting

--- src/concurrency/producerconsumer/queues/ToastManual.java
+++ src/concurrency/producerconsumer/queues/ToastManual.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by bogdan.teut on 16/10/2014.
  */
-public class ToastManul {
+public class ToastManual {
     public static void main(String[] args) {
         ToastCoordinator toastCoordinator = new ToastCoordinator();
     }
