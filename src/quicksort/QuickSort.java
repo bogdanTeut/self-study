b7f11be52a20ad6572f2ccda2a632577255c0c28
bteut <bogdan_teut@yahoo.com>
2014-11-26 22:37:45 +0000

    Quiciksort

--- src/quicksort/QuickSort.java
+++ src/quicksort/QuickSort.java
@@ -4,19 +4,27 @@ package quicksort;
  * Created by bogdan.teut on 25/11/2014.
  */
 public class QuickSort {
-    public static int[] sort(int[] array, left, right) {
+    public static int[] sort(int[] array, int left, int right) {
+        if (left > right) return null;
+        int index = partition(array, left, right);
+        sort(array, left, index - 1);
+        sort(array, index + 1, right);
+        return array;
+    }
+
+    private static int partition(int[] array, int left, int right) {
         int pivot = array[right];
-        int posIndex = 0;
-        for (int i=0;i<array.length-1;i++){
+        int posIndex = left;
+        for (int i=left;i<right;i++){
             if (array[i] < pivot){
                 swap(array, i, posIndex++);
             }
         }
 
-        swap(array, posIndex, array.length-1);
-        posIndex++;
+        swap(array, posIndex, right);
+//        posIndex++;
 
-        return array;
+        return posIndex;
     }
 
     private static void swap(int[] array, int x, int y) {
--- src/quicksort/QuickSortTest.java
+++ src/quicksort/QuickSortTest.java
@@ -24,12 +24,22 @@ public class QuickSortTest {
     }
 
     @Test
-    public void whenPartiallyUnsortedArrayAndReapplyingAlgorithmIsNecessary(){
+    public void whenReapplyingAlgorithmIsNecessaryLowerIndex(){
         Assert.assertEquals(new int[]{0, 1, 2}, sort(new int[]{1, 0, 2}));
     }
 
+    @Test
+    public void whenReapplyingAlgorithmIsNecessaryUpperIndex(){
+        Assert.assertEquals(new int[]{0, 1, 2}, sort(new int[]{1, 2, 0}));
+    }
+
+    @Test
+    public void whenReapplyingAlgorithmIsNecessaryBothSides(){
+        Assert.assertEquals(new int[]{ 1, 2, 3, 4, 5, 5, 7 ,8 ,9}, sort(new int[]{3, 8, 7, 4, 5, 2, 1, 9, 5}));
+    }
+
     private int[] sort(int[] array) {
-        return QuickSort.sort(array);
+        return QuickSort.sort(array, 0, array.length-1);
     }
 
 
